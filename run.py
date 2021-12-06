d24140edba98bc6727207c8f62f6d2f918d0571f
Nemanja <nemanja@arch.cloud>
2021-12-06 19:51:36 +0100

    init

--- run.py
+++ run.py
@@ -1,8 +1,5 @@
 import boto3
-from flask import Flask, render_template, request, send_from_directory
-from dashboard import get_dashboard_data
-from logs import get_logs_data
-from settings import get_settings_data
+from flask import Flask, render_template
 
 app = Flask(__name__)
 dynamodb = boto3.client('dynamodb', endpoint_url='http://localhost:8000')
@@ -14,22 +11,15 @@ def index():
 
 @app.route('/dashboard')
 def dashboard():
-   return render_template('home/dashboard.html', dashboard_data = get_dashboard_data(dynamodb))
+   return render_template('home/dashboard.html')
  
-@app.route('/settings', methods=('GET', 'POST'))
+@app.route('/settings')
 def settings():
-   return render_template('home/settings.html', settings_data = get_settings_data(request, dynamodb))
+   return render_template('home/settings.html')
 
 @app.route('/logs')
 def logs():
-   return render_template('home/logs.html', logs_data = get_logs_data(request))
-
-@app.route('/logs/download')
-def logs_download():
-   filename = request.args.get('filename')
-   if '-' in filename:
-      filename = filename.replace('-', '#')
-   return send_from_directory(directory="../log/", path=filename)
+   return render_template('home/logs.html')
    
 if __name__ == '__main__':
    app.run(debug = True)
